6902632fecb701b8ce0235037e5159ddd81787d8
Newman Posey <newman@newmans-mbp.attlocal.net>
2017-11-03 02:27:54 -0400

    I am struggling if you cant tell

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+alert("You have 9 guesses to guess what letter I am thinking of! Good luck...");
+
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var secLetter = [Math.floor(Math.random() * letters.length)];
+
+
+
+var guessCount = 0;	
+
+// function userGuess() {
+//     var guess = document.getElementById("fname").value;
+//     document.getElementById("demo").innerHTML = x;
+// } <------This was a failed attempt at a function------->
+
+
+
+// Computer generates random number/letter between 1-26 or a-z
+
+	
+
+// Prompt User to guess a random letter between a-z
+
+
+
+
+
+// Check to see if answer is correct
+
+// If guess is wrong, display the guess on the screen up to 9 guesses
+
+// If guess is correct before the 9 guesses are up add a point to the win column, if not add a point to the loss column
+
+// Prompt message telling User they won
+
+
+
+
+
+
