cf7e10a7d6b75dfad0648bbe027c4d5ba1b40a16
Newman Posey <newman@newmans-mbp.attlocal.net>
2017-11-04 01:22:48 -0400

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+
 alert("You have 9 guesses to guess what letter I am thinking of! Good luck...");
 
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -5,19 +6,14 @@ var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
 var wins = 0;
 
 var losses = 0;
+var maxGuesses = 9;
+var guessesLeft = maxGuesses;
+var guesses = "";
 
-var guesses =0;
 
-var guessesLeft = 9;
 
 var secNum = Math.floor(Math.random() * letters.length);
 console.log(secNum);
-
-
-
-
-
-
 
 
 	var compGuess = letters[secNum];
@@ -27,25 +23,37 @@ console.log(secNum);
 	document.onkeyup = function(event){
 
 		var userGuess = event.key;
+		if (guessesLeft != maxGuesses) {
+		guesses= guesses +  ", " + userGuess;}
+		document.getElementById("guesses").innerHTML =  "Your Guesses So Far: " + guesses;
+
 
 		if (userGuess === compGuess) {
 
 			alert("you win!");
 			wins++;
+			guessesLeft = maxGuesses;
+			guesses = "";
+			document.getElementById("wins").innerHTML =  "Wins: " + wins;
+
+			// Print to console.  Ask of quit/play?
 		}
 
-		else if (userGuess !== compGuess) {
-				guessesLeft++;
+		else  {
+				guessesLeft--;
+				document.getElementById("guesses-left").innerHTML =  "Guesses Left: " + guessesLeft;
 
-		}
+			if (guessesLeft < 1) {
+				losses++;
+				guessesLeft = maxGuesses;
+				guesses = "";
+				
+				document.getElementById("losses").innerHTML =  "Losses: " + losses;
 
-		else if (guessesLeft < 1) {
 
-			losses++;
-
-			alert("you lost...")
-		}
+			}}
 	};
+
 
 
 
